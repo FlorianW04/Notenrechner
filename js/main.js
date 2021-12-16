3c3dc34404985a6242817f72fb04f00b8d6d5ce2
wickerfl <florian10wickert@gmail.com>
2021-12-16 09:35:27 +0100

    h

--- js/main.js
+++ js/main.js
@@ -1,90 +1,21 @@
-let n1, n2, n3, n4, n5;
-let anzahlNoten= 6;
+function percentage_1() {
 
-function noteRechnen() {
-    let ergebnis;
-    n1= parseInt(document.getElementById("eingabe1").value);
-    n2= parseInt(document.getElementById("eingabe2").value);
-    n3= parseInt(document.getElementById("eingabe3").value);
-    n4= parseInt(document.getElementById("eingabe4").value);
-    n5= parseInt(document.getElementById("eingabe5").value);
-    n6= parseInt(document.getElementById("eingabe6").value);
+    // Method returns the element of percent id
+    var percent = document.getElementById("percent").value;
 
-    console.log(n1 + ";" + n2 + ";" + n3 + ";" + n4 + ";" + n5 + ";" + n6)
-   ergebnis= summeBerechnen()/anzahlNoten;
-    console.log(ergebnis);
-    document.getElementById( "durchschnitt").innerHTML = ergebnis;
-    if(ergebnis < 5){
-        document.getElementById("durchschnitt").style.color = "crimson";
-    }
-    if(ergebnis >= 5){
-        document.getElementById("durchschnitt").style.color = "#74e968";
-    }
-    anzahlNoten = 6;
-    return ergebnis;
+    // Method returns the element of num id
+    var num = document.getElementById("num").value;
+    document.getElementById("value1")
+        .value = (num / 100) * percent;
 }
 
-function summeBerechnen(){
-    let summe = 0;
-    if(isNaN(n1)){
-        console.log(summe);
-        console.log("n1 ist 0");
-        anzahlNoten = anzahlNoten - 1;
-        console.log("anzahlNoten:" + anzahlNoten);
-    }else{
-        summe = summe + n1
-        console.log("Summe ist:" + summe);
-    }
+function percentage_2() {
 
-    if(isNaN(n2)){
-        console.log(summe);
-        console.log("n2 ist 0");
-        anzahlNoten = anzahlNoten - 1;
-        console.log("anzahlNoten:" + anzahlNoten);
-    }else{
-        summe = summe + n2
-        console.log("Summe ist:" + summe);
-    }
+    // Method returns the element of num1 id
+    var num1 = document.getElementById("num1").value;
 
-    if(isNaN(n3)){
-        console.log(summe);
-        console.log("n3 ist 0");
-        anzahlNoten = anzahlNoten - 1;
-        console.log("anzahlNoten:" + anzahlNoten);
-    }else{
-        summe = summe + n3
-        console.log("Summe ist:" + summe);
-    }
-
-    if(isNaN(n4)){
-        console.log(summe);
-        console.log("n4 ist 0");
-        anzahlNoten = anzahlNoten - 1;
-        console.log("anzahlNoten:" + anzahlNoten);
-    }else{
-        summe = summe + n4
-        console.log("Summe ist:" + summe);
-    }
-
-    if(isNaN(n5)){
-        console.log(summe);
-        console.log("n5 ist 0");
-        anzahlNoten = anzahlNoten - 1;
-        console.log("anzahlNoten:" + anzahlNoten);
-    }else{
-        summe = summe + n5
-        console.log("Summe ist:" + summe);
-    }
-
-    if(isNaN(n6)){
-        console.log(summe);
-        console.log("n6 ist 0");
-        anzahlNoten = anzahlNoten - 1;
-        console.log("anzahlNoten:" + anzahlNoten);
-    }else{
-        summe = summe + n6
-        console.log("Summe ist:" + summe);
-    }
-
-
+    // Method returns the elements of num2 id
+    var num2 = document.getElementById("num2").value;
+    document.getElementById("value2")
+        .value = (num1 * 100) / num2 + "%";
 }
